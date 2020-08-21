79ce1dcd88276b1883760c91cdb77024a4973163
Stepan Khomyn <skhomyn@uwo.ca>
2020-08-21 00:33:39 -0400

    - Git ignore .env file. - Use env port variable for API. - Code styling cleanup.

--- app.js
+++ app.js
@@ -1,60 +1,59 @@
-// const http = require('http');
-const fetch = require('node-fetch');
+require("dotenv").config();
 
-var express = require("express");
-var app = express();
+const fetch = require("node-fetch");
+const express = require("express");
 
-fetch('https://api.ratesapi.io/api/latest')
-    .then(res => res.json())
-    .then(json => console.log(json));
+const app = express();
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
-});
-
-app.get('/currencies', async (req, res) => {
+/**
+ * [GET] Get latest currencies.
+ */
+app.get("/currencies", async (req, res) => {
     try {
-        const request = await fetch('https://api.ratesapi.io/api/latest');
+        const request = await fetch("https://api.ratesapi.io/api/latest");
 
         const response = await request.json();
 
         res.json({
-            data: response
+            data: response,
         });
     } catch (error) {
         res.status(500).json({
-            message: 'Internal error',
-            data: {}
+            message: "Internal error",
+            data: {},
         });
     }
 });
 
-app.get('/currencies/:currency([A-Z]{3,3})', async (req, res) => {
+/**
+ * [GET] Get currency rate by currency code.
+ */
+app.get("/currencies/:currency([A-Z]{3,3})", async (req, res) => {
     try {
-        const request = await fetch('https://api.ratesapi.io/api/latest');
+        const request = await fetch("https://api.ratesapi.io/api/latest");
 
         const response = await request.json();
 
         if (!response.rates[req.params.currency]) {
             return res.status(422).json({
-                message: 'Invalid currency.',
-                data: {}
-            })
+                message: "Invalid currency.",
+                data: {},
+            });
         }
 
         res.json({
             data: {
-                [req.params.currency]: response.rates[req.params.currency]
-            }
+                [req.params.currency]: response.rates[req.params.currency],
+            },
         });
     } catch (error) {
         res.status(500).json({
-            message: 'Internal error',
-            data: {}
+            message: "Internal error",
+            data: {},
         });
     }
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server running on port 8080");
 });
